015983fe44f0bd404b89af2e429b9476bd804306
PACHAIYAPPAN.M <18008412-mjsysudar@users.noreply.replit.com>
2024-08-18 14:10:45 +0000

    Initial commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+    <h1 className="text-danger">404-Page Not Found</h1>
+        </div>
+        </div>
+    </main>  
+  )
+}
+export default NotFound;
